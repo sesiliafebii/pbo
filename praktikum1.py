eea6a02eb67a46f46437d88bbf5d21a9ec120861
sesiliafebii <163487400+sesiliafebii@users.noreply.github.com>
2024-03-15 09:19:01 +0700

    Add files via upload

---
+++ praktikum1.py
@@ -0,0 +1,20 @@
+#no 1
+#progam dengan menggunakan fungsi print() dan input()
+
+print("biodata diri saya")
+
+nama = input("nama :")
+NIM = int(input("NIM :"))
+alamat = input("alamat :")
+angkatan = int(input("angkatan :"))
+jurusan = input("jurusan :")
+fakultas = input("fakultas :")
+universitas = input("universitas :")
+
+print("nama :", nama)
+print("NIM :", NIM)
+print("alamat :", alamat)
+print("angkatan :", angkatan)
+print("Sistem Informasi :", jurusan)
+print("fakultas :", fakultas)
+print("universitas :", universitas)
